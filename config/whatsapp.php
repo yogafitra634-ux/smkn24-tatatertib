97d4f61e66b5aa8d6497bf402765cb82353f2a01
tatatertib <yogafitra634@gmail.com>
2026-06-07 00:06:35 +0700

    Update whatsapp.php

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -2,7 +2,7 @@
 
 function kirimWA($nomor, $pesan)
 {
-    $token = 'GnWUa6uZedzxHLPHGmGi';
+    $token = 'FawuDLs1Yr8bZjcYUr6S';
 
     $nomor = preg_replace('/[^0-9]/', '', $nomor);
 
@@ -16,17 +16,9 @@ function kirimWA($nomor, $pesan)
         CURLOPT_URL => 'https://api.fonnte.com/send',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_POST => true,
-
-        CURLOPT_CONNECTTIMEOUT => 30,
-        CURLOPT_TIMEOUT => 60,
-
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_SSL_VERIFYHOST => false,
-
         CURLOPT_HTTPHEADER => [
             'Authorization: ' . $token
         ],
-
         CURLOPT_POSTFIELDS => [
             'target'  => $nomor,
             'message' => $pesan
@@ -34,25 +26,8 @@ function kirimWA($nomor, $pesan)
     ]);
 
     $response = curl_exec($curl);
-    $error    = curl_error($curl);
-    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
     curl_close($curl);
 
-    error_log("=== FONNTE DEBUG ===");
-    error_log("Nomor: " . $nomor);
-    error_log("HTTP Code: " . $httpCode);
-
-    if ($error) {
-        error_log("cURL Error: " . $error);
-    }
-
-    error_log("Response: " . $response);
-
-    return [
-        'success'  => empty($error),
-        'httpcode' => $httpCode,
-        'response' => $response,
-        'error'    => $error
-    ];
+    return $response;
 }
