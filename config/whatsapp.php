5a9d42f5d3f8548fc4a5505334e0deca38de2b4c
tatatertib <yogafitra634@gmail.com>
2026-06-07 00:10:05 +0700

    Update whatsapp.php

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -16,9 +16,17 @@ function kirimWA($nomor, $pesan)
         CURLOPT_URL => 'https://api.fonnte.com/send',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_POST => true,
+
+        CURLOPT_CONNECTTIMEOUT => 30,
+        CURLOPT_TIMEOUT => 60,
+
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+
         CURLOPT_HTTPHEADER => [
             'Authorization: ' . $token
         ],
+
         CURLOPT_POSTFIELDS => [
             'target'  => $nomor,
             'message' => $pesan
@@ -26,8 +34,25 @@ function kirimWA($nomor, $pesan)
     ]);
 
     $response = curl_exec($curl);
+    $error    = curl_error($curl);
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
     curl_close($curl);
 
-    return $response;
+    error_log("=== FONNTE DEBUG ===");
+    error_log("Nomor: " . $nomor);
+    error_log("HTTP Code: " . $httpCode);
+
+    if ($error) {
+        error_log("cURL Error: " . $error);
+    }
+
+    error_log("Response: " . $response);
+
+    return [
+        'success'  => empty($error),
+        'httpcode' => $httpCode,
+        'response' => $response,
+        'error'    => $error
+    ];
 }
